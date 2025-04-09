f65c6e99bff492a9890c1a9c824c26787b23b815
Ramal Abeysekera <ramal.abeysekera@hotmail.com>
2025-04-09 20:45:30 +0100

    comments add for AWS config init

--- config/configs..go
+++ config/configs..go
@@ -1,3 +1,4 @@
+// Package config provides AWS configuration functionality
 package config
 
 import (
@@ -6,9 +7,12 @@ import (
     "os"
 )
 
+// AwsConfig holds the AWS configuration settings
 var AwsConfig aws.Config
 
+// init initializes the AWS configuration when command line arguments are provided
 func init() {
+    // Only load config if command line args exist
     if len(os.Args) > 1 {
         AwsConfig = helpers.LoadAwsConfig()
     }
