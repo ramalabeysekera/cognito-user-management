b9d21af34f62786e89529028d660b8ed1d2cb9c0
Ramal Abeysekera <ramal.abeysekera@hotmail.com>
2025-04-10 19:44:13 +0100

    createUser operation moved as a helper func

---
+++ pkg/common/createUser.go
@@ -0,0 +1,47 @@
+package common
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
+)
+
+func CreateUser(userPoolId string, userName string, tempPassword string, permpass bool, AwsConfig aws.Config) error {
+
+	// Initialize Cognito client with AWS configuration
+	cogClient := cognitoidentityprovider.NewFromConfig(AwsConfig)
+
+	// Prepare user creation input parameters
+	userInput := cognitoidentityprovider.AdminCreateUserInput{
+		UserPoolId:        &userPoolId,
+		Username:          &userName,
+		MessageAction:     types.MessageActionTypeSuppress,
+		TemporaryPassword: &tempPassword,
+	}
+
+	// Set timeout context for AWS API call
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Create user in Cognito
+	AdminCreateUserOutput, err := cogClient.AdminCreateUser(ctx, &userInput)
+
+	// Handle user creation response
+	if err != nil {
+		return err
+	} else {
+		if permpass {
+			log.Println("User created successfully")
+		} else {
+			log.Println("User created successfully")
+			log.Printf("Username: %s, UserStatus: %s",
+				*AdminCreateUserOutput.User.Username,
+				AdminCreateUserOutput.User.UserStatus)
+		}
+	}
+	return nil
+}
