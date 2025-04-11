aed7d785916c055cb5d2cc1adb73fa909c13160a
Ramal Abeysekera <ramal.abeysekera@hotmail.com>
2025-04-11 21:26:22 +0100

    banner add

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,44 @@
 /*
 Copyright © 2025 Ramal Abeysekera ramal.abeysekera@hotmail.com
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 )
 
+const banner = `
+   ____  _    _ __  __ 
+  / ___|| |  | |  \/  |
+ | |    | |  | | |\/| |
+ | |___ | |__| | |  | |
+  \____| \____/|_|  |_|
 
+Cognito User Management
+Author: Ramal Abeysekera
+Version: 1.1.0
+`
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-    Use:   "cognitousermanagement",
-    Short: "A CLI tool for managing AWS Cognito users",
-    Long: `CognitoUserManagement is a command-line application designed to simplify 
+	Use:   "cognitousermanagement",
+	Short: "A CLI tool for managing AWS Cognito users",
+	Long: `CognitoUserManagement is a command-line application designed to simplify 
 the management of users in AWS Cognito user pools. 
 
 With this tool, you can perform various operations such as creating, updating, 
 deleting, and listing users, as well as managing groups and user attributes. 
 It is built to streamline user management tasks for developers and administrators 
 working with AWS Cognito.`,
-    // Uncomment the following line if your bare application
-    // has an action associated with it:
-    // Run: func(cmd *cobra.Command, args []string) { },
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println(banner)
+	},
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +61,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
