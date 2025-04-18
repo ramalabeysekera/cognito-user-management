61902fcacd9c751bcf141b3d93e0304ad4a0f7de
Ramal Abeysekera <ramal.abeysekera@hotmail.com>
2025-04-18 18:57:19 +0100

    add delete user function to common pkg

---
+++ pkg/common/deleteUser.go
@@ -0,0 +1,33 @@
+package common
+
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+)
+
+func DeleteUser(awsConfig aws.Config, userPoolId string, userName string) error {
+	// Initialize Cognito client with AWS configuration
+	cogClient := cognitoidentityprovider.NewFromConfig(awsConfig)
+
+	// Prepare user deletion input parameters
+	userInput := cognitoidentityprovider.AdminDeleteUserInput{
+		UserPoolId:        &userPoolId,
+		Username:          &userName,
+	}
+
+	// Set timeout context for AWS API call
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Delete user in Cognito
+	_ , err := cogClient.AdminDeleteUser(ctx, &userInput)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
