e9c0aa72f6a4513b2f71c1fe0cf5ca5ddc50126f
Ramal Abeysekera <ramal.abeysekera@hotmail.com>
2025-04-25 20:29:25 +0100

    add helper func to print red/green logs

---
+++ pkg/helpers/printLog.go
@@ -0,0 +1,16 @@
+package helpers
+
+import (
+	"github.com/fatih/color"
+	"log"
+)
+
+func PrintSuccessLog(message string) {
+	green := color.New(color.FgGreen).SprintFunc()
+	log.Print(green(message))
+}
+
+func PrintFatalErrorLog(message string) {
+	red := color.New(color.FgRed).SprintFunc()
+	log.Fatal(red(message))
+}
